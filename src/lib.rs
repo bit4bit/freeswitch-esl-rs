f0c91070437031493f248e6a6d02fa8b8a6a020e
bit4bit <>
2022-10-08 14:28:47 +0000

    add LICENSE
    FossilOrigin-Name: a13ad62b8fa5cfc3edfc3cdd7a62812066a67c48c88d03653b7c642187024def

---
+++ src/lib.rs
@@ -0,0 +1,225 @@
+use std::{io, str, error, fmt};
+use std::io::{Read, Write, BufReader};
+
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Pdu {
+    header: HashMap<String, String>,
+    // TODO: no pub
+    pub content: Vec<u8>
+}
+
+#[derive(Debug)]
+pub enum PduError {
+    IOError(io::Error)
+}
+
+impl fmt::Display for PduError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            PduError::IOError(e) => write!(f, "{}", e)
+        }
+    }
+}
+
+impl error::Error for PduError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match self {
+            PduError::IOError(e) => Some(e)
+        }
+    }
+}
+
+impl From<io::Error> for PduError {
+    fn from(e: io::Error) -> Self {
+        PduError::IOError(e)
+    }
+}
+
+impl Pdu {
+    pub fn parse(reader: &mut impl io::BufRead) -> Result<Pdu, PduError> {
+        let mut pdu = Pdu {
+            header: HashMap::new(),
+            content: Vec::new()
+        };
+
+        pdu.do_parse(reader)?;
+        
+        Ok(pdu)
+    }
+
+    fn do_parse(&mut self, reader: &mut impl io::BufRead) -> io::Result<()> {
+        self.parse_header(reader)?;
+        self.parse_content(reader)?;
+
+        Ok(())
+    }
+
+    fn get_header_content(&self, reader: &mut impl io::BufRead) -> io::Result<Vec<u8>> {
+        let mut raw: Vec<u8> = Vec::new();
+        let mut buf: Vec<u8> = Vec::new();
+
+        loop {
+            buf.clear();
+            let readed_bytes = reader.read_until(b'\n', &mut buf)?;
+            if readed_bytes == 1 && buf[0] == b'\n' {
+                break;
+            } else {
+                raw.append(&mut buf);
+            }
+        }
+
+        Ok(raw)
+    }
+
+    fn parse_header(&mut self, reader: &mut impl io::BufRead) -> io::Result<()> {
+        let raw = self.get_header_content(reader)?;
+        let raw_str = String::from_utf8(raw).unwrap();
+        let parts = raw_str
+            .split("\n")
+            .filter(|line| {
+                line.bytes().count() > 0
+            })
+            .map(|line| {
+                let mut item = line.splitn(2, ":");
+                let key = item.next().unwrap();
+                let value = item.next().unwrap();
+
+                (key.trim(), value.trim()) 
+            });
+        
+        for (key, value) in parts {
+            self.header.insert(key.to_string(), value.to_string());
+        }
+
+        Ok(())
+    }
+
+    fn parse_content(&mut self, reader: &mut impl io::BufRead) -> io::Result<()> {
+        if let Some(length) = self.header.get("Content-Length") {
+            let length: usize = length.parse().unwrap();
+            let mut content = vec![0u8; length];
+            reader.read_exact(&mut content)?;
+
+            self.content.append(&mut content);
+        }
+
+        Ok(())
+    }
+}
+
+pub struct Connection<C: Read + Write> {
+    reader: BufReader<C>
+}
+
+impl<C: Read + Write> Connection<C> {
+    pub fn new(inner: C) -> Connection<C> {
+        let reader = BufReader::new(inner);
+        
+        Self {
+            reader
+        }
+    }
+
+    pub fn reader(&mut self) -> &mut BufReader<C> {
+        &mut self.reader
+    }
+
+    pub fn connection(&mut self) -> &mut C  {
+        self.reader.get_mut()
+    }
+}
+
+
+
+pub struct Client<T: Write + Read> {
+    connection: Connection<T>,
+    event_queue: Vec<Pdu>
+}
+
+impl<T: Read + Write> Client<T> {
+    pub fn new(connection: Connection<T>) -> Self {
+        Self{
+            connection,
+            event_queue: Vec::new()
+        }
+    }
+
+    pub fn pull_event(&mut self) -> Result<Pdu, PduError> {
+        loop {
+            let pdu = Pdu::parse(self.connection.reader())?;
+            
+            if pdu.header["Content-Type"] == "text/event-plain" {
+                return Ok(pdu);
+            }
+        }
+    }
+
+    pub fn event(&mut self, event: &str) -> Result<(), PduError> {
+        self.send_command(format_args!("event plain {}", event))?;
+        Ok(())
+    }
+    
+    pub fn api(&mut self, cmd: &str, arg: &str) -> Result<Pdu, PduError> {
+        self.send_command(format_args!("api {} {}", cmd, arg))?;
+
+        let pdu = self.wait_for_response("api/response")?;
+
+        Ok(pdu)
+    }
+
+    pub fn auth(&mut self, pass: &str) -> Result<(), &'static str> {
+        let pdu = Pdu::parse(self.connection.reader()).unwrap();
+        
+        if pdu.header["Content-Type"] == "auth/request" {
+            self.send_command(format_args!("auth {}", pass)).unwrap();
+
+            let pdu = Pdu::parse(self.connection.reader()).unwrap();
+            if pdu.header["Reply-Text"] == "+OK accepted" {
+                Ok(())
+            } else {
+                Err("fails to authenticate")
+            }
+        } else {
+            Err("fails to authenticate")
+        }
+    }
+
+    fn wait_for_response(&mut self, content_type: &str) -> Result<Pdu, PduError> {
+        loop {
+            let pdu = Pdu::parse(self.connection.reader())?;
+            if pdu.header["Content-Type"] == content_type {
+                return Ok(pdu)
+            } else {
+                self.event_queue.push(pdu);
+            }
+        }
+    }
+
+    fn send_command(&mut self, cmd: std::fmt::Arguments) -> io::Result<()> {
+        self.connection.connection().write(format!("{}\n\n", cmd).as_bytes())?;
+        self.connection.connection().flush()?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_authenticate() -> Result<(), &'static str> {
+        use std::io::Cursor;
+        let mut protocol = Cursor::new(vec![0; 512]);
+        protocol.write_fmt(format_args!("Content-Type: auth/request\n\n")).unwrap();
+        protocol.write_fmt(format_args!("Reply-Text: +OK accepted\n\n")).unwrap();
+        protocol.set_position(0);
+
+        let conn = Connection::new(&mut protocol);
+        let mut client = Client::new(conn);
+
+        client.auth("test")?;
+        Ok(())
+    }
+}
