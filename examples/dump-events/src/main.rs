5c7a08a29c82fd1f487eb3427eec407ceb9e98b1
bit4bit <>
2022-10-15 20:14:30 +0000

    increase event buffer size
    FossilOrigin-Name: d2e64c577a80f024090b229ade6d97173fc87af5d62af7199fa5b7da9f6ccba8

--- examples/dump-events/src/main.rs
+++ examples/dump-events/src/main.rs
@@ -3,20 +3,37 @@ use std::net::{TcpStream};
 use std::env;
 use freeswitch_esl_rs::{Connection,Client,Event};
 
+#[derive(Debug)]
+struct Stats {
+    events_per_sec: u64
+}
+
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let host = &args[1];
+    let event = &args[2];
     let mut stream = TcpStream::connect(host)?;
 
     let conn = Connection::new(&mut stream);
     let mut client = Client::new(conn);
+    let mut stats = Stats{events_per_sec: 0};
 
     client.auth("cloudpbx").expect("fails to authenticate");
-    client.event("ALL").expect("fails enabling events");
+    client.event(event).expect("fails enabling events");
+
+    let mut now = std::time::Instant::now();
+    let notify_on_secs = std::time::Duration::from_secs(1);
 
     loop {
         let event: Event = client.pull_event().unwrap();
-        println!("{:?}", event);
+        stats.events_per_sec += 1;
+        
+        if now.elapsed() >= notify_on_secs {
+            println!("{:?} {:?}", now.elapsed(), stats);
+
+            now = std::time::Instant::now();
+            stats.events_per_sec = 0;
+        }
     }
     
     Ok(())
