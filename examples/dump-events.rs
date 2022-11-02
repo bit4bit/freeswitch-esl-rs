a60973e0cb2481f5f21559aa4483d80dd831682a
bit4bit <bit4bit@riseup.net>
2022-11-02 01:23:19 +0000

    update example
    FossilOrigin-Name: 73890959d72c8941ba5e5888ae13d83b486a33737f9433373cdb6b4e6318127a

--- examples/dump-events.rs
+++ examples/dump-events.rs
@@ -1,10 +1,14 @@
+extern crate freeswitch_esl_rs;
 use std::net::{TcpStream};
 use std::env;
+use std::collections::HashMap;
+use std::time::SystemTime;
 use freeswitch_esl_rs::{Connection,Client,Event};
 
 #[derive(Debug)]
 struct Stats {
-    events_per_sec: u64
+    event_per_tick: u64,
+    events_counts_per_sec: HashMap<String, u64>
 }
 
 fn main() -> std::io::Result<()> {
@@ -15,7 +19,10 @@ fn main() -> std::io::Result<()> {
 
     let conn = Connection::new(stream);
     let mut client = Client::new(conn);
-    let mut stats = Stats{events_per_sec: 0};
+    let mut stats = Stats{
+        event_per_tick: 0,
+        events_counts_per_sec: HashMap::new()
+    };
 
     client.auth("cloudpbx").expect("fails to authenticate");
     client.event(event).expect("fails enabling events");
@@ -25,13 +32,35 @@ fn main() -> std::io::Result<()> {
 
     loop {
         let event: Event = client.pull_event().unwrap();
-        stats.events_per_sec += 1;
-        
+        let event_name = event.get("Event-Name").unwrap().to_string();
+
+        stats.event_per_tick += 1;
+        if event_name == "CUSTOM" {
+            let event_subclass = event.get("Event-Subclass").unwrap().to_string();
+            let mut event_name = format!("{}:{}", event_name, event_subclass);
+            if let Some(action) = event.get("CC-Action") {
+                event_name = format!("{}:{}", event_name, action.to_string())
+            }
+            if let Some(counter) = stats.events_counts_per_sec.get_mut(&event_name) {
+                *counter += 1;
+            } else {
+                stats.events_counts_per_sec.insert(event_name, 1);
+            }
+        } else {
+            if let Some(counter) = stats.events_counts_per_sec.get_mut(&event_name) {
+                *counter += 1;
+            } else {
+                stats.events_counts_per_sec.insert(event_name, 1);
+            }
+        }
+
         if now.elapsed() >= notify_on_secs {
-            println!("{:?} {:?}", now.elapsed(), stats);
+            let stamp = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+            println!("{} {:?} {:?}", stamp.as_secs(), now.elapsed(), stats);
 
             now = std::time::Instant::now();
-            stats.events_per_sec = 0;
+            stats.event_per_tick = 0;
+            stats.events_counts_per_sec.clear();
         }
     }
     
